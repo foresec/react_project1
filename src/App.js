7843b9f13341fa8b4fb684f25c134bdee0685fa2
박소현 <foresec@naver.com>
2022-12-27 17:10:49 +0900

    repair

--- src/App.js
+++ src/App.js
@@ -1,23 +1,155 @@
-import React, { useEffect } from "react";
-import Login from "./components/Login";
-import Spotify from "./components/Spotify";
+import React, {useCallback, useMemo, useEffect, useRef, useReducer, useState} from "react"
 
-import { reducerCases } from "./utils/Constants";
-import { useStateProvider } from "./utils/StateProvider";
+import './App.css';
+import DiaryEditor from './DiaryEditor';
+import DiaryList from './DiaryList';
+// import OptimizeTest from "./OptimizeTest";
+// import Lifecycle from './Lifecycle';
 
-const App = () => {
-  const [{ token }, dispatch] = useStateProvider();
+import DoughnutGraph from './DoughnutGraph'
 
-  useEffect(() => {
-    const hash = window.location.hash;
-    if (hash) {
-      // # 제거, & 사이로 나누고 0번째~ 어쩌구가 토큰
-      const token = hash.substring(1).split("&")[0].split("=")[1];
-      dispatch({ type: reducerCases.SET_TOKEN, token });
+
+const reducer = (state, action) => {
+  switch(action.type){
+
+    case 'INIT': {
+      return action.data
     }
-  }, [token, dispatch]);
 
-  return <div>{token ? <Spotify /> : <Login />}</div>;
-};
+    case 'CREATE': {
+      const created_date = new Date().getTime()
+      const newItem = {
+        ...action.data,
+        created_date
+      }
+      return [newItem, ...state]
+    } 
+
+    case 'REMOVE': {
+      return state.filter((it)=>it.id !== action.targetId)
+    } 
+    case 'EDIT': {
+      return state.map((it) => 
+        it.id === action.targetId ? 
+        {...it, content: action.newContent} : it
+      )
+    }
+    default :
+    return state
+  }
+}
+
+export const DiaryStateContext = React.createContext()
+
+export const DiaryDispatchContext = React.createContext()
+
+function App() {
+
+  const [data, dispatch]=useReducer(reducer, [])
+
+  const dataId = useRef(0) 
+  
+  const getData = async() => {
+    const res = await fetch(
+      'https://jsonplaceholder.typicode.com/comments'
+      ).then((res) => res.json())
+     
+      const initData = res.slice(0,20).map((it)=>{
+        return {
+          author: it.email,
+          content: it.body,
+          emotion: Math.floor(Math.random() * 5) + 1, 
+          created_date : new Date().getTime(),
+          id: dataId.current++
+        }
+      })
+  
+    dispatch({type:"INIT", data: initData})
+  }
+
+  useEffect(()=>{
+    getData()
+  },[])
+
+
+  const onCreate = useCallback((author, content, emotion)=>{
+
+    dispatch({type:"CREATE", data:{
+      author,
+      content,
+      emotion,
+      id:dataId.current,
+    }})
+
+    dataId.current += 1       
+  },[])
+
+
+  const onRemove = useCallback((targetId) => {
+    dispatch({type:"REMOVE", targetId})
+  },[])
+
+
+  const onEdit = useCallback((targetId, newContent) => {
+    dispatch({type:"EDIT", targetId, newContent})
+  }, [])
+
+
+
+  const memoizedDispatches = useMemo(()=> {
+    return {onCreate, onRemove, onEdit}
+  },[])
+
+
+  const getDiaryAnalysys = useMemo(() => {
+    const goodCount = data.filter((it)=>it.emotion >= 3).length
+    const badCount = data.length - goodCount
+    const goodRatio = (goodCount / data.length) * 100
+    return {goodCount, badCount, goodRatio}
+  }, [data.length]
+  )
+
+  const {goodCount, badCount, goodRatio} = getDiaryAnalysys
+
+
+
+  const [isEditor, setIsEditor] = useState(false)
+  const toggleIsEditor = () => setIsEditor(!isEditor)
+
+
+
+  // 자식요소에 추가
+  return (
+    <DiaryStateContext.Provider value={data}>
+      <DiaryDispatchContext.Provider value={memoizedDispatches}>
+        <div className="App">
+          {/* <OptimizeTest/> */}
+          {/* <Lifecycle /> */}
+          {isEditor ? (
+            <><button onClick={toggleIsEditor}>HOME</button></>
+            ) : (
+            <><button onClick={toggleIsEditor}>START</button></>
+          )}
+          <div className="container">
+            {isEditor ? (
+              <>
+                <DiaryEditor onCreate={onCreate}/>
+              </>
+              ) : (
+              <div className="DoughnutContainer"><DoughnutGraph /></div>
+            )}
+          </div>
+          <div>
+            <div>전체 일기 : {data.length}</div>
+            <div>기분 좋은 일기 개수 : {goodCount}</div>
+            <div>기분 나쁜 일기 개수 : {badCount}</div>
+            <div>기분 좋은 일기 비율 : {goodRatio}</div>
+          </div>
+          <DiaryList onEdit={onEdit} onRemove={onRemove} />
+        </div>
+      </DiaryDispatchContext.Provider>
+    </DiaryStateContext.Provider>
+  )
+}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import { StateProvider } from "./utils/StateProvider";
-import reducer, { initialState } from "./utils/reducer";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 
-ReactDOM.render(
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <StateProvider initialState={initialState} reducer={reducer}>
-      <App />
-    </StateProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />
+  </React.StrictMode>
 );
+
+
